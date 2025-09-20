b4b61cf2b9d864bde1826b4dba33ecca6d0f9efe
dadyownes15 <mikkeljanusdahl@gmail.com>
2025-09-20 17:54:40 +0200

    updated config

--- src/hpc_env.py
+++ src/hpc_env.py
@@ -52,7 +52,7 @@ class HPCenv(Env):
         if self.mode == "training":
             self.workload_path = "data/workloads/training_workload.swf"
         if self.mode == "validation":
-            self.config_dict["episode_length"] = 300 # This value is hardcoded to be the length of the whole validation set
+            self.config_dict["episode_length"] = 5901 # This value is hardcoded to be the length of the whole validation set
             self.workload_path = "data/workloads/validation_workload.swf"
         if self.mode == "test":
             self.config_dict["episode_length"] = 54811 # this is hardcoded to be the length of the whole test set
