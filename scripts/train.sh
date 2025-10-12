f1fa4970c4f79f56243f5544627d3d8dd932eed4
dadyownes15 <mikkeljanusdahl@gmail.com>
2025-10-12 14:02:56 +0200

    updated trainign script

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=sweep
 #SBATCH --array=1-7%7           
 #SBATCH --cpus-per-task=8
-#SBATCH --time=2:00:00
+#SBATCH --time=5:00:00
 
 export WANDB_API_KEY="418d10fc7ab5763a7e2ec89f2dc5aed81c38bd8e"
 # Define log file (timestamped)
@@ -10,10 +10,10 @@ LOGFILE="run_$(date +%Y%m%d_%H%M%S).log"
 
 
 # Define eta values
-etas=(1.0 0.0 0.75 0.5 0.25 0.9 0.95 0.99)
+etas=(1 0 0.75 0.5 0.25 0.9 0.95 0.99)
 
 # Select eta based on the SLURM array ID
-eta=0.0
+eta=${etas[$SLURM_ARRAY_TASK_ID-1]}
 
 echo "Running with eta=$eta"
 
