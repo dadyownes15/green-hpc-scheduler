c80edcef019c3ce2eb2ca39be82ab775a770bb5b
dadyownes15 <mikkeljanusdahl@gmail.com>
2025-10-13 09:20:03 +0200

    ready for sweep

--- scripts/sweep_eta.sh
+++ scripts/sweep_eta.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 #The partition is the queue you want to run on. standard is gpu and can be ommitted.
-#SBATCH -p gpu
 #SBATCH --job-name=eta_sweep
 #SBATCH --array=1-6%3 
-#number of independent tasks we are going to start in this script
-#SBATCH --ntasks=1
 #number of cpus we want to allocate for each program
 #SBATCH --cpus-per-task=8
 #We expect that our program should not run longer than 2 days
 #Note that a program will be killed once it exceeds this time!
 #SBATCH --time= 12:00:00
-#Skipping many options! see man sbatch
-# From here on, we can start our program
+
 
 set -e
 
