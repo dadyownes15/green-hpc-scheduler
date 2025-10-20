5a592725483a0635f8422d0e79a88ae9b8986924
dadyownes15 <mikkeljanusdahl@gmail.com>
2025-10-20 09:17:17 +0200

    added sweep script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=sweep
-#SBATCH --array=1-6%6           
+#SBATCH --array=1-1%1           
 #SBATCH --cpus-per-task=16
 #SBATCH --time=7:00:00
 
@@ -10,7 +10,7 @@ LOGFILE="run_$(date +%Y%m%d_%H%M%S).log"
 
 
 # Define eta values
-etas=(0.75 0.5 0.25 0.01 0.001)
+etas=(0.75)
 
 # Select eta based on the SLURM array ID
 eta=${etas[$SLURM_ARRAY_TASK_ID-1]}
